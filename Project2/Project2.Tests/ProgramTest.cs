67558d8fd6f1c393698d5083eef6dcf8d30ac028
Alain Nacario <alain.nacario@ptml.com>
2017-01-19 14:45:33 +1300

    initial commit master

---
+++ Project2/Project2.Tests/ProgramTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Project2.Tests
+{
+    public class ProgramTest
+    {
+        [Test]
+        public void TestVersion()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Project2/Project2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        private static string VersionNo = "0.02";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("verison: {0}", VersionNo));
+        }
+    }
+}
