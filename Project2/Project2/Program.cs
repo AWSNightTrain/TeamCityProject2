e87019175e64a4d7f22f9946507fe2ee06cca0c5
Alain Nacario <alain.nacario@ptml.com>
2017-01-24 14:48:22 +1300

    Up Version

--- Project2/Project2/Program.cs
+++ Project2/Project2/Program.cs
@@ -8,7 +8,7 @@ namespace Project2
 {
     class Program
     {
-        private static string VersionNo = "0.02";
+        private static string VersionNo = "0.03";
         static void Main(string[] args)
         {
             Console.WriteLine(string.Format("verison: {0}", VersionNo));
